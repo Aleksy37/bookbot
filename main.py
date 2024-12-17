a8637493e7828aa2c75ab7bedd23aeba98717f39
Aleksy37 <aleksyveillette@gmail.com>
2024-12-17 13:02:17 -0500

    added book report

--- main.py
+++ main.py
@@ -3,13 +3,33 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_char_dict(text)
-    print(text)
-    print(num_words)
-    print(chars_dict)
+    freq_report = get_freq_report(chars_dict)
+    book_report = get_book_report(freq_report)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+    print(book_report)
+    print("--- End Report ---")
+
+def get_book_report(freq_report):
+    report = []
+    for i in freq_report:
+        char = i["char"]
+        freq = i["freq"]
+        report.append(f"The '{char}' character was found {freq} times")
+    return "\n".join(report)
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_freq_report(chars_dict):
+    dict_list = []
+    for char in chars_dict:
+        if char.isalpha():
+            dict_list.append({"char": char, "freq": chars_dict[char]})
+    dict_list.sort(reverse= True, key= lambda dict_list: dict_list["freq"])
+    return dict_list
 
 def get_char_dict(text):
     chars = {}
